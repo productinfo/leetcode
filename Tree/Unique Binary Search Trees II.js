1caf2c221e5ee213d9dcdc6fc4ae6dc16c511571
LeoAJ <leo0924@gmail.com>
2015-04-24 12:48:34 -0700

    upload

---
+++ Tree/Unique Binary Search Trees II.js
@@ -0,0 +1,59 @@
+/**
+ * Definition for binary tree
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.min = this.max = null;
+ * }
+ */
+
+// time O(n^2)
+// not compile
+/**
+ * @param {number} n
+ * @returns {TreeNode[]}
+ */
+var generateTrees = function(n) {
+
+  
+
+  var go = function (min, max) {
+
+    var res = [];
+
+    if (min > max) {
+      res.push(null);
+      return res;
+    }
+
+    if (min === max) {
+      res.push(new TreeNode(min));
+      return res;
+    }
+
+    for (var i = min ; i <= max ; i++) {
+
+      var lefts = go(min, i - 1);
+      var rights = go(i + 1, max);
+
+      for (var j = 0 ; j < lefts.length ; j++) {
+        for (var k = 0 ; k < rights.length ; k++) {
+
+          var node = new TreeNode(i);
+
+          node.left = lefts[j];
+          node.right = rights[k];
+
+          res.push(node);
+
+        }
+
+      }
+    }
+
+  };
+
+  return go(1, n);
+
+  // return res;
+
+};
