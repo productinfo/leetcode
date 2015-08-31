2229f98ef4c617fe97bc52207a3a28a5ec7047ab
LeoAJ <leo0924@gmail.com>
2015-08-31 13:14:54 -0700

    upload

---
+++ Javascript/isEqual.js
@@ -0,0 +1,24 @@
+var isEqual = function (obj1, obj2) {
+
+	var key1 = Object.keys(obj1),
+			key2 = Object.keys(obj2);
+
+	if (key1.length !== key2.length) return false;
+
+	for (var k1 in obj1) {
+
+		if (!obj2[k1]) return false;
+
+		if (typeof obj1[k1] !== typeof obj2[k1]) return false;
+
+		if (typeof obj1[k1] === 'object') {
+			if (!compare(obj1[k1], obj2[k1])) return false;
+		} else {
+			if (obj1[k1] !== obj2[k1]) return false;
+		}
+
+	}
+
+	return true;
+
+};
