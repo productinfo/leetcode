c82e687706797577037c030156bfdb061360f5af
LeoAJ <leo0924@gmail.com>
2015-10-07 15:24:08 -0700

    upload

---
+++ Tree/Closest Binary Search Tree Value.js
@@ -0,0 +1,55 @@
+/**
+Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+
+Note:
+
+Given target value is a floating point.
+You are guaranteed to have only one unique value in the BST that is closest to the target.
+*/
+
+var closestValue = function (root, target) {
+
+  if (!root || !target) return Number.MAX_VALUE;
+
+  if (!root.left && !root.right) return root.val;
+
+  var l = closestValue(root.left, target),
+      r = closestValue(root.right, target),
+      diff = Math.abs(root.val - target),
+      ld = Math.abs(l - target),
+      rd = Math.abs(r - target);
+
+  if (diff < ld) {
+    return diff < rd ? root.val : r;
+  } else {
+    return ld < rd ? l : r;
+  }
+
+};
+
+
+/////////////////////////////////////////////
+//
+/////////////////////////////////////////////
+
+var closestValue = function (root, target) {
+
+  if (!root || !target) return null;
+
+  if (root.val === target) return root;
+  else if (root.val < target) {
+
+    if (!root.left) return root;
+    var temp = closestValue(root.left, target);
+    return temp.val > root.val ? root : temp;
+
+  } else {
+
+    if (!root.right) return root;
+    var temp = closestValue(root.right, target);
+    return temp.val > root.val ? root : temp;
+
+  }
+
+  return null;
+};
