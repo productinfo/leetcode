2747c1e4e10914be43d75a612e80d6e50310e011
LeoAJ <leo0924@gmail.com>
2015-11-10 10:56:48 -0800

    upload

--- string/Roman to Integer.js
+++ string/Roman to Integer.js
@@ -1,41 +1,35 @@
-function map (char) {
-  
-  switch (char) {
-
-    case 'I':
-      return 1;
-    case 'V':
-      return 5;
-    case 'X':
-      return 10;
-    case 'L':
-      return 50;
-    case 'C':
-      return 100;
-    case 'D':
-      return 500;
-    case 'M':
-      return 1000;
-    default:
-      return 0;
-
-  }
-
-}
-
 /**
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-  var r = 0;
-  var l = s.length;
-  for (var i = 0 ; i < l ; i++) {
-    r += map(s[i]);
-    if (i > 0 && map(s[i]) > map(s[i - 1])) {
-      r -= 2 * map(s[i - 1]);
-    }
-  }
+var map = {
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  'C': 100,
+  'D': 500,
+  'M': 1000
+};
 
-  return r;
+var romanToInt = function(s) {
+
+    if (!s) return -1;
+    var len = s.length;
+    if (len === 0) return 0;
+
+    var i = 0, total = 0;
+
+    for (; i < len ; i++) {
+
+        total += map[s[i]];
+
+        if (i > 0 && map[s[i]] > map[s[i - 1]]) {
+            total -= 2 * map[s[i - 1]];
+        }
+
+    }
+
+    return total;
+
 };
