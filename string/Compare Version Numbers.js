73c84ced492f1f9b85b3c84838686f4caff1a970
LeoAJ <leo0924@gmail.com>
2015-11-29 21:49:53 -0800

    upload

--- string/Compare Version Numbers.js
+++ string/Compare Version Numbers.js
@@ -4,19 +4,19 @@
  * @return {number}
  */
 var compareVersion = function(version1, version2) {
-  
+
   var index = 0, v1 = version1.split('.'), v2 = version2.split('.'), t1, t2;
 
   while (v1[index] || v2[index]) {
 
-    t1 = parseInt(v1[index] || 0);
-    t2 = parseInt(v2[index] || 0);
-    index++;
+    t1 = parseInt(v1[index], 10) || 0;
+    t2 = parseInt(v2[index], 10) || 0;
     if (t1 < t2) {
       return -1;
     } else if (t1 > t2) {
       return 1;
     }
+    index++;
   }
 
   return 0;
