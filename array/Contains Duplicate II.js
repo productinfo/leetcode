36b8ca40418507f1aad538d9d1b30c78d38b7990
LeoAJ <leo0924@gmail.com>
2015-12-27 20:08:10 -0800

    upload

--- array/Contains Duplicate II.js
+++ array/Contains Duplicate II.js
@@ -1,41 +1,29 @@
 /**
- * Given an array of integers and an integer k, find out whether there there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.
- * 
  * @param {number[]} nums
  * @param {number} k
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    
-  if (!nums) {
-    return false;
-  }
+
+  if (k < 1 || !nums) return false;
 
   var len = nums.length;
 
-  if (len === 0) {
-    return false;
-  }
+  if (len === 0) return false;
 
-  var map = {}, i = 0, j, diff;
+  var i = 0, map = {};
 
   for (; i < len ; i++) {
 
     if (map.hasOwnProperty(nums[i])) {
 
-      j = map[nums[i]];
-
-      diff = i - j;
-
-      map[nums[i]] = i;
-
-      if (diff <= k) {
+      if (i - map[nums[i]] <= k) {
         return true;
       }
 
-    } else {
-      map[nums[i]] = i;
     }
+
+    map[nums[i]] = i;
 
   }
 
