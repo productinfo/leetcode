84a11e3a56e42812342abb8cf9d877ae7b4b9e53
LeoAJ <leo0924@gmail.com>
2016-01-20 21:04:03 -0800

    upload

--- Tree/Binary Tree Upside Down.js
+++ Tree/Binary Tree Upside Down.js
@@ -1,3 +1,48 @@
+/**
+Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node) or empty, flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
+For example:
+Given a binary tree {1,2,3,4,5},
+    1
+   / \
+  2   3
+ / \
+4   5
+return the root of the binary tree [4,5,2,#,#,3,1].
+   4
+  / \
+ 5   2
+    / \
+   3   1
+   */
+
+// time `O(logn)`
+
+var upsideDownBinaryTree = function(root) {
+
+ if (!root) return null;
+
+ var l = root.left;
+ var r = root.right;
+ var newR = upsideDownBinaryTree(l);
+
+ if (l) {
+   l.left = r;
+   l.right = root;
+ }
+
+ root.left = null;
+ root.right = null;
+
+ return newR ? newR : root;
+
+};
+
+
+/////////////////////////////////////////
+//
+/////////////////////////////////////////
+
+
 var upsideDownBinaryTree = function (root) {
 
   if (!root) return null;
