b4a23e0110f75585fd53c5a5dd4c334cf4056ae7
LeoAJ <leo0924@gmail.com>
2016-02-12 22:30:20 +0800

    upload

--- Math/Palindrome Number .js
+++ Math/Palindrome Number .js
@@ -37,3 +37,42 @@ var isPalindrome = function(x) {
   return true;
 
 };
+
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+
+  if (x < 0) {
+    return false;
+  }
+
+  if (x < 10) {
+    return true;
+  }
+
+  var s = x + '';
+
+  var start = 0, last = s.length - 1;
+
+  while (start < last) {
+
+    if (s[start] !== s[last]) {
+      return false;
+    }
+
+    start++;
+    last--;
+
+  }
+
+  return true;
+
+};
