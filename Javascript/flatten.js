0e0caf4db55a98d0b2a1ccafa31455c051b4794a
LeoAJ <leo0924@gmail.com>
2016-03-01 21:20:08 -0800

    upload

--- Javascript/flatten.js
+++ Javascript/flatten.js
@@ -45,3 +45,21 @@ var f = function (arr) {
 	return result;
 
 };
+
+// 2/29/2016
+
+var foo = function (a) {
+  var r = [];
+	while(a.length > 0) {
+		var t = a.shift();
+		if (Array.isArray(t)) {
+			a = t.concat(a);
+		} else {
+			r.push(t);
+		}
+	}
+	
+	return r;
+};
+
+console.log(foo([1, 2, [3, [4, 5, [6], 7], 8], 9]));
