1e1b472b9ad786320fcc019e5caef521d663e716
LeoAJ <leo0924@gmail.com>
2016-04-03 15:44:57 -0700

    upload

--- array/Maximum Subarray.js
+++ array/Maximum Subarray.js
@@ -5,7 +5,7 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-  
+
   var l = nums.length;
 
   var i = 0;
@@ -30,4 +30,28 @@ var maxSubArray = function(nums) {
 
   return max;
 
+};
+
+// 3/29/2016
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  var len = nums.length;
+  var sum = 0;
+  var max = Number.NEGATIVE_INFINITY;
+  var t;
+
+  while (len--) {
+	  t = nums[len];
+    sum += t;
+    if (sum < t) {
+      sum = t;
+    }
+    if (sum > max) {
+      max = sum;
+    }
+  }
+  return max;
 };
