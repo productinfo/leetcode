c3414c72660bd333eb7e8937e7d83bb2b079872e
LeoAJ <leo0924@gmail.com>
2016-04-08 10:07:25 -0700

    upload

--- array/Contains Duplicate.js
+++ array/Contains Duplicate.js
@@ -7,25 +7,42 @@ var containsDuplicate = function(nums) {
     if (!nums) {
         return false;
     }
-    
+
     var len = nums.length;
-    
+
     if (len === 0) {
         return false;
     }
-    
+
     var i = 0, map = {};
-    
+
     for (; i < len ; i++) {
-        
+
         if (map.hasOwnProperty(nums[i])) {
             return true;
         }
-        
+
         map[nums[i]] = true;
-        
+
     }
-    
+
     return false;
 
+};
+
+// 4/7/2016
+const containsDuplicate = (n) => {
+
+  let len = n.length;
+  const map = {};
+
+  while (len--) {
+    if (map[n[len]]) {
+      return true;
+    } else {
+      map[n[len]] = true;
+    }
+  }
+
+  return false;
 };
