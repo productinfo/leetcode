22749fda31d2878e086b91f01016f9f4ab20c5a4
LeoAJ <leo0924@gmail.com>
2016-04-23 12:37:22 -0700

    upload

--- array/Pascal's Triangle.js
+++ array/Pascal's Triangle.js
@@ -35,3 +35,28 @@ var generate = function(numRows) {
   return result;
 
 };
+
+// 4/19/2016
+var generate = function(n) {
+  if (n < 1) {
+    return [];
+  }
+
+  var r = [[1]];
+
+  if (n === 1) {
+    return r;
+  }
+
+  for (var i = 1; i < n; i++) {
+    var pv = r[i - 1];
+    var cur = [1];
+    for (var j = 1; j < pv.length; j++) {
+      cur.push(pv[j] + pv[j - 1]);
+    }
+    cur.push(1);
+    r.push(cur);
+  }
+
+  return r;
+};
