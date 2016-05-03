e6157ab54802125e0784f6f12d1712cf7838de6d
LeoAJ <leo0924@gmail.com>
2016-05-03 09:08:58 -0700

    upload

--- string/Valid Palindrome.js
+++ string/Valid Palindrome.js
@@ -22,11 +22,11 @@ var isPalindrome = function(x) {
   }
 
   return true;
-    
+
 };
 
 function is (ch) {
-  
+
   if (ch >= 'a' && ch <= 'z') {
     return true;
   }
@@ -37,3 +37,46 @@ function is (ch) {
   return false;
 
 }
+
+// 4/4/2016
+const check = (c) => {
+
+  if ('0' <= c && c <= '9') {
+    return true;
+  }
+
+  if ('a' <= c && c <= 'z') {
+    return true;
+  }
+
+  return false;
+};
+
+const isPalindrome = (s) => {
+
+  const len = s.length;
+
+  if (!len) return true;
+
+  s = s.toLowerCase().trim();
+
+  let p1 = 0, p2 = len - 1;
+
+  while (p1 < p2) {
+
+    if (!check(s[p1])) {
+      p1++;
+    } else if (!check(s[p2])) {
+      p2--;
+    } else if (s[p1] === s[p2]) {
+      p1++;
+      p2--;
+    } else {
+      return false;
+    }
+
+  }
+
+  return true;
+
+};
