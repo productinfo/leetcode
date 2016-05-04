0fec58eb096e08d65696b49521fec1ba9aa4da39
LeoAJ <leo0924@gmail.com>
2016-05-04 15:11:58 -0700

    upload

--- string/Multiply Strings.js
+++ string/Multiply Strings.js
@@ -47,3 +47,52 @@ var multiply = function(num1, num2) {
   return carry ? carry + res : res;
 
 };
+
+// 3/28/2016
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+
+  var res = '';
+
+  if (!num1 || !num2) return res;
+
+  if (num1 === '0' || num2 === '0') return '0';
+
+  var n1 = num1.length,
+      n2 = num2.length,
+      i, j, a, b, tl, sum,
+      carry = 0, tmp = [];
+
+  for (i = 0; i < n1 ; i++) {
+
+    a = parseInt(num1[i], 10);
+
+    for (j = 0 ; j < n2 ; j++) {
+
+      b = parseInt(num2[j], 10);
+
+      if (tmp[i + j]) {
+        tmp[i + j] += a * b;
+      } else {
+        tmp[i + j] = a * b;
+      }
+
+    }
+
+  }
+
+  tl = tmp.length;
+
+  while (tl--) {
+    sum = +(tmp[tl]) + carry;
+    res = (sum % 10) + res;
+    carry = (sum / 10) >> 0;
+  }
+
+  return carry ? carry + res : res;
+
+};
