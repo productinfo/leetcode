15c2f09a71cdfd1ab57b943a664e1a1feecf1050
LeoAJ <leo0924@gmail.com>
2016-05-06 08:54:47 -0700

    upload

--- string/Length of Last Word.js
+++ string/Length of Last Word.js
@@ -22,3 +22,22 @@ var lengthOfLastWord = function(s) {
   return l - 1 - end;
 
 };
+
+// 3/30/2016
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+
+  if (!s || !s.length) return 0;
+  s = s.trim();
+  var end = s.length - 1;
+  var index = end;
+
+  while (index >= 0 && s[index] !== ' ') {
+    index--;
+  }
+
+  return end - index;
+};
