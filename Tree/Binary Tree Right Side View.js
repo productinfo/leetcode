dfd5eabebec00d57dcc1dd7aa5adfaac9a27f18b
LeoAJ <leo0924@gmail.com>
2016-05-15 05:23:20 +0800

    upload

--- Tree/Binary Tree Right Side View.js
+++ Tree/Binary Tree Right Side View.js
@@ -29,15 +29,15 @@ var rightSideView = function(root) {
   while (q.length > 0) {
 
     size = q.length;
-    
+
     for (j = 0 ; j < size ; j++) {
-      
+
       node = q.shift();
-      
+
       if (j === 0) {
         res.push(node.val);
       }
-      
+
       if (node.right) {
         q.push(node.right);
       }
@@ -50,4 +50,45 @@ var rightSideView = function(root) {
   }
 
   return res;
+};
+
+// 4/7/2016
+// space O(1)
+const rightSideView = (root) => {
+
+	const list = [];
+
+	const go = (r, level) => {
+		if (!r) return;
+
+		if (list.length === level) list.push(r.val);
+
+		go(r.right, level + 1, list);
+		go(r.left, level + 1, list);
+	};
+
+	go(root, 0);
+	return list;
+};
+
+// space O(n)
+"use strict";
+
+var rightSideView = function rightSideView(root) {
+
+	var r = [];
+	if (!root) return r;
+
+	var q = [root];
+
+	while (q.length > 0) {
+		for (var j = 0; j < q.length; j++) {
+			var node = q.shift();
+			if (j === 0) r.push(node.val);
+			if (node.right) q.push(node.right);
+			if (node.left) q.push(node.left);
+		}
+	}
+
+	return r;
 };
