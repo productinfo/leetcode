746ead1dfe44f52c072846fa77ed8bc2468fae3f
LeoAJ <leo0924@gmail.com>
2016-05-18 16:51:03 +0800

    upload

--- Myself/Construct a Special Triangle from a Given Array.js
+++ Myself/Construct a Special Triangle from a Given Array.js
@@ -29,3 +29,43 @@ var printTriangle = function (arr) {
 
   return res;
 };
+
+// 4/8/2016
+const printTriangle = (arr) => {
+
+	const len = arr.length;
+
+  const res = [arr];
+
+	for (let i = 0; i < len - 1; i++) {
+
+		const pv = res[0];
+		const cur = [];
+
+		for (let j = 0; j < pv.length - 1; j++) {
+			cur[j] = pv[j] + pv[j + 1];
+		}
+
+		res.unshift(cur);
+
+	}
+
+	return res
+};
+
+// 4/16/2016
+const foo = (n) => {
+
+  const r = [n];
+
+  while (r[0] && r[0].length > 1) {
+    const pv = r[0];
+    const t = [];
+    for (let i = 1; i < pv.length; i++) {
+      t.push(pv[i - 1] + pv[i]);
+    }
+    r.unshift(t);
+  }
+
+  return r;
+};
