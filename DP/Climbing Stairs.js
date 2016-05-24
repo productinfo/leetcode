cc75109facaa3c1e545c53dc4a80ea8c3db0bf3f
LeoAJ <leo0924@gmail.com>
2016-05-24 19:13:40 +0800

    upload

--- DP/Climbing Stairs.js
+++ DP/Climbing Stairs.js
@@ -2,6 +2,9 @@
  * @param {number} n
  * @return {number}
  */
+
+// time: O(n)
+// space: O(n)
 var climbStairs = function(n) {
 
   var ans = [1, 1];
@@ -17,21 +20,38 @@ var climbStairs = function(n) {
 };
 
 function x (n, dd) {
-  
+
   var arr = dd || [];
-  
+
   if (n === 0) {
     console.log(arr.join(''));
   }
-  
+
   if (n >= 1) {
       x(n - 1, ['1'].concat(arr));
   }
-  
+
   if (n >= 2) {
       x(n - 2, ['2'].concat(arr));
   }
-  
+
 }
 
 x(3);
+
+// time: O(n)
+// space: O(1)
+// 3/31/2016
+const climbStairs = (x) => {
+  let d1 = 1;
+  if (x < 2) return d1;
+  let d2 = 2;
+  if (x < 3) return d2;
+  let r, i = 3;
+  while (i++ <= x) {
+    r = d1 + d2;
+    d1 = d2;
+    d2 = r;
+  }
+  return r;
+};
