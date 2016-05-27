c1f940029a88d3706996c44840c9e9ebf2827d9c
LeoAJ <leo0924@gmail.com>
2016-05-27 19:57:11 +0800

    upload

--- Javascript/events.js
+++ Javascript/events.js
@@ -45,3 +45,46 @@ export let events = {
   }
 
 };
+
+// 3/9/2016
+const events = {
+
+	events: {},
+
+	on(name, callback) {
+    this.events[name] = this.events[name] || [];
+		this.events[name].push(callback);
+	},
+
+	off(name, callback) {
+    if (this.events[name]) {
+			const index = this.events[name].indexOf(callback);
+      if (index !== -1) {
+				this.events[name].splice(index, 1);
+			}
+		}
+	},
+
+	emit(name, data) {
+		if (this.events[name]) {
+			this.events[name].forEach((callback) => {
+				callback(data);
+			});
+		}
+	}
+
+};
+
+// // console.log(event);
+// const f = (x) => {
+//   console.log(x);
+// };
+//
+// const g = (x) => {
+//   console.log(x + 1);
+// };
+//
+// events.on('leo', f);
+// events.emit('leo', 1);
+// events.off('leo', f);
+// events.emit('leo', 1);
