f0c5d6fd60fd2060d513fa46b5ca18e328f8d5e2
LeoAJ <leo0924@gmail.com>
2016-05-31 15:08:00 +0800

    upload

--- Javascript/flatten.js
+++ Javascript/flatten.js
@@ -47,7 +47,6 @@ var f = function (arr) {
 };
 
 // 2/29/2016
-
 var foo = function (a) {
   var r = [];
 	while(a.length > 0) {
@@ -58,8 +57,52 @@ var foo = function (a) {
 			r.push(t);
 		}
 	}
-	
+
 	return r;
 };
 
 console.log(foo([1, 2, [3, [4, 5, [6], 7], 8], 9]));
+
+// 4/27/2016
+const x = [1, 2, [3], [4, [5, [6, 7, 8], 9], 10], 11];
+
+const f = (arr, re) => {
+
+	let len = arr.length;
+	let index = 0;
+
+	while (len--) {
+		const elem = arr[index++];
+		if (Array.isArray(elem)) {
+			f(elem, re);
+		} else {
+			re.push(elem);
+		}
+	}
+
+};
+
+
+// solution 1:
+const a = (arr) => {
+	const r = [];
+	f(arr, r);
+	return r;
+};
+
+// solution 2:
+const b = (arr) => {
+	const r = [];
+  while (arr.length > 0) {
+		const elem = arr.shift();
+		if (Array.isArray(elem)) {
+      arr = elem.concat(arr);
+		} else {
+			r.push(elem);
+		}
+	}
+	return r;
+};
+
+console.log(a(x));
+console.log(b(x));
