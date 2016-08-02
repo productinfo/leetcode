b23313758157e9ca5789757fd424729abf8923e6
lhsieh1 <lhsieh@walmartlabs.com>
2016-08-01 18:57:12 -0700

    upload

--- Javascript/isArray.js
+++ Javascript/isArray.js
@@ -1,3 +1,6 @@
 var isArray = Array.isArray || function (obj) {
   return Object.prototype.toString.call(obj) === ['object Array'];
 };
+
+// ES6
+const isArray = Array.isArray || ((obj) => Object.prototype.toString.call(obj) === '[object Array]');
