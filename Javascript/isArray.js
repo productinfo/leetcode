0c6d6535aa22a9ef3e830860c9cfbe407ae30e57
LeoAJ <leo0924@gmail.com>
2016-08-01 18:59:02 -0700

    upload

--- Javascript/isArray.js
+++ Javascript/isArray.js
@@ -2,5 +2,6 @@ var isArray = Array.isArray || function (obj) {
   return Object.prototype.toString.call(obj) === ['object Array'];
 };
 
+// 2016/8/1
 // ES6
 const isArray = Array.isArray || ((obj) => Object.prototype.toString.call(obj) === '[object Array]');
