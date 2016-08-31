1e9bc5c016f2b7d2ee3c3d298d9f795a3b617982
LeoAJ <leo0924@gmail.com>
2016-08-30 21:51:33 -0700

    upload

--- Tree/Binary Tree Maximum Path Sum.js
+++ Tree/Binary Tree Maximum Path Sum.js
@@ -41,3 +41,46 @@ var maxPathSum = function(root) {
 
   return max;
 };
+
+// 8/29/2016
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const getLargest = (...arg) => arg.reduce((prev, next) => {
+  if (prev > next) {
+    return prev;
+  }
+  return next;
+});
+
+const maxPathSum = function(root) {
+
+  if (!root) {
+    return 0;
+  }
+
+  let max = Number.NEGATIVE_INFINITY;
+  const go = rt => {
+    if (!rt) {
+      return 0;
+    }
+
+    const l = go(rt.left);
+    const r = go(rt.right);
+    const v = rt.val;
+    const mx1 = getLargest(l + v, r + v, v);
+    const mx2 = getLargest(l + r + v, mx1, v);
+    max = getLargest(max, mx2);
+    return mx1;
+  };
+  go(root);
+  return max;
+};
