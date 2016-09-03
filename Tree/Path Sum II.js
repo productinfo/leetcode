d74aaa8af40bff5f34a1929d37d076ee537d6993
LeoAJ <leo0924@gmail.com>
2016-09-02 18:04:53 -0700

    upload

--- Tree/Path Sum II.js
+++ Tree/Path Sum II.js
@@ -50,3 +50,53 @@ var pathSum = function(root, sum) {
   return result;
 
 };
+
+// 9/2/2016
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number[][]}
+ */
+var pathSum = function(root, sum) {
+
+  const result = [];
+
+  if (!root) return result;
+
+  const sub = [];
+
+  const dfs = (r, remain) => {
+
+    if (!r) return;
+
+    sub.push(r.val);
+
+    if (!r.left && !r.right && remain === r.val) {
+      result.push(sub.slice());
+      return;
+    }
+
+    if (r.left) {
+      dfs(r.left, remain - r.val);
+      sub.pop();
+    }
+
+    if (r.right) {
+      dfs(r.right, remain - r.val);
+      sub.pop();
+    }
+
+  };
+
+  dfs(root, sum);
+
+  return result;
+
+};
