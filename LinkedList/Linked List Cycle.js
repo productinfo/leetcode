b8d941b4f358bea12fac884194d2cfb1b0b21381
LeoAJ <leo0924@gmail.com>
2016-09-05 20:28:00 -0700

    upload

--- LinkedList/Linked List Cycle.js
+++ LinkedList/Linked List Cycle.js
@@ -11,7 +11,7 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-  
+
   if (!head) {
     return false;
   }
@@ -27,4 +27,24 @@ var hasCycle = function(head) {
   }
 
   return false;
+};
+
+// 9/4/2016
+var hasCycle = function(head) {
+
+  if (!head || !head.next) return false;
+
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+
 };
