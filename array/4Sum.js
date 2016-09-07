f322f4464b769bf065739dd3bc189d4979cde8ae
LeoAJ <leo0924@gmail.com>
2016-09-06 21:13:18 -0700

    upload

--- array/4Sum.js
+++ array/4Sum.js
@@ -114,3 +114,37 @@ var fourSum = function(nums, target) {
 
   return r;
 };
+
+// 9/5/2016
+const fourSum = (nums, target) => {
+  const res = [];
+  const len = nums.length;
+  if (!len) return res;
+
+  nums = nums.sort((a, b) => a - b);
+
+  for (let a = 0; a < len - 3; a++) {
+    if (a > 0 && nums[a] === nums[a - 1]) continue;
+    for (let b = a + 1; b < len - 2; b++) {
+      if (b > a + 1 && nums[b] === nums[b - 1]) continue;
+      let c = b + 1;
+      let d = len - 1;
+      while (c < d) {
+        const sum = nums[a] + nums[b] + nums[c] + nums[d];
+        if (target === sum) {
+          res.push([nums[a], nums[b], nums[c], nums[d]]);
+          while (c < d && nums[c] === nums[c + 1]) c++;
+          while (c < d && nums[d] === nums[d - 1]) d--;
+          c++;
+          d--;
+        } else if (sum < target) {
+          c++;
+        } else {
+          d--;
+        }
+      }
+    }
+  }
+
+  return res;
+};
