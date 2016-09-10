ae02975f258ef1648d7df236f0eb772790d9bd5e
LeoAJ <leo0924@gmail.com>
2016-09-09 17:03:09 -0700

    upload

---
+++ Myself/Subset Sum Problem.js
@@ -0,0 +1,29 @@
+/*
+Detect if a subset from a given set of N non-negative integers sums upto a given value S.
+Example:
+Set: {1, 3, 9, 2}, S = 5
+Output: true
+*/
+
+const goo = (nums, target) => {
+	const len = nums.length;
+	const total = 0;
+	let flag = false;
+	nums = nums.sort((a, b) => a - b);
+	const dfs = (index, rem) => {
+		if (rem === 0) {
+			flag = true;
+			return;
+		}
+		for (let i = index; i < len; i++) {
+			if (nums[i] > rem) {
+				return;
+			}
+			dfs(i + 1, rem - nums[i])
+		}
+	};
+	dfs(0, target);
+	return flag;
+};
+
+console.log(goo([1, 3, 9, 2], 5));
