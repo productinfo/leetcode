0cc29d91695d69c618995028f674d83178b14fb3
LeoAJ <leo0924@gmail.com>
2016-09-13 22:38:57 -0700

    upload

--- Myself/Print All Diagonals of a given matrix.js
+++ Myself/Print All Diagonals of a given matrix.js
@@ -32,3 +32,35 @@ const foo = matrix => {
 };
 
 foo([[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]);
+
+// 9/6/2016
+const log = (...arg) => console.log.apply(console, arg);
+
+const foo = m => {
+	const yl = m.length;
+	const xl = m[0].length;
+	let x, y, t;
+	y = 0;
+	while (y < yl) {
+		x = 0;
+		t = y;
+		while (t >= 0) {
+			log(m[t][x]);
+			t--;
+			x++;
+		}
+		y++;
+	}
+
+	x = 1;
+	while (x < xl) {
+		y = yl - 1;
+		t = x;
+		while (t < xl) {
+			log(m[y][t]);
+			t++;
+			y--;
+		}
+		x++;
+	}
+};
