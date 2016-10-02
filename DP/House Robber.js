013160e19418ce382f59174b67a1673b694737f7
LeoAJ <leo0924@gmail.com>
2016-10-02 10:48:57 -0700

    upload

--- DP/House Robber.js
+++ DP/House Robber.js
@@ -25,3 +25,15 @@ var rob = function(nums) {
   return dp[l - 1];
 
 };
+
+// 9/25/2016
+var rob = function(nums) {
+  const len = nums.length;
+  if (!len) return 0;
+  if (len === 1) return nums[0];
+  const dp = [nums[0], Math.max(nums[0], nums[1])];
+  for (let i = 2; i < len; i++) {
+    dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+  }
+  return dp[len - 1];
+};
