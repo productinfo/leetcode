f08a4cefa85170880db2a72bf0f43c4e9f3b6db0
LeoAJ <leo0924@gmail.com>
2016-10-03 11:03:18 -0700

    upload

--- Hash/Isomorphic Strings.js
+++ Hash/Isomorphic Strings.js
@@ -24,7 +24,7 @@ var valid = function (s1, s2) {
       // match
       if (map[s1[i]] !== s2[i]) {
         return false;
-      }  
+      }
     } else {
       // not match
       map[s1[i]] = s2[i];
@@ -35,3 +35,23 @@ var valid = function (s1, s2) {
   return true;
 
 };
+
+// 9/26/2016
+var isIsomorphic = function(s, t) {
+  const l1 = s.length;
+  const l2 = t.length;
+  if (l1 !== l2) return false;
+  return valid(s, t) && valid(t, s);
+};
+
+const valid = (s1, s2) => {
+  const map = {};
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i] in map) {
+      if (map[s1[i]] !== s2[i]) return false;
+    } else {
+      map[s1[i]] = s2[i];
+    }
+  }
+  return true;
+}
