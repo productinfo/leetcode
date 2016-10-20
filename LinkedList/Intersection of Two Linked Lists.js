a19090c6b41ef7ebc871d20862750e3b8a4844fb
LeoAJ <leo0924@gmail.com>
2016-10-20 13:05:57 -0700

    upload

--- LinkedList/Intersection of Two Linked Lists.js
+++ LinkedList/Intersection of Two Linked Lists.js
@@ -63,3 +63,40 @@ var getIntersectionNode = function(headA, headB) {
 
   return null;
 };
+
+// 10/6/2016
+const getIntersectionNode = (h1, h2) => {
+  let l1 = 0;
+  let l2 = 0;
+  let c1 = h1;
+  let c2 = h2;
+  while (c1) {
+    l1++;
+    c1 = c1.next;
+  }
+  while (c2) {
+    l2++;
+    c2 = c2.next;
+  }
+  let p1;
+  let p2;
+  if (l1 > l2) {
+    p1 = h1;
+    p2 = h2;
+  } else {
+    p1 = h2;
+    p2 = h1;
+  }
+  let step = Math.abs(l1 - l2);
+  while (step--) {
+    p1 = p1.next;
+  }
+  while (p1 && p2) {
+    if (p1 === p2) {
+      return p1;
+    }
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+  return null;
+};
