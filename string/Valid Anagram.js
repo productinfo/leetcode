9d6fddf35161c5ac90c2a4fd81da2f56840f960c
LeoAJ <leo0924@gmail.com>
2016-11-22 10:14:21 -0800

    upload

--- string/Valid Anagram.js
+++ string/Valid Anagram.js
@@ -37,3 +37,52 @@ const isAnagram = (s, t) => {
 
   return true;
 };
+
+// 9/15/2016
+var isAnagram = function(s, t) {
+  const l1 = s.length;
+  const l2 = t.length;
+  if (l1 !== l2) return false;
+  const map = {};
+  for (let i = 0; i < l1; i++) {
+    if (map[s[i]]) {
+      map[s[i]]++;
+    } else {
+      map[s[i]] = 1;
+    }
+  }
+  for (let i = 0; i < l2; i++) {
+    if (map[t[i]]) {
+      map[t[i]]--;
+    } else {
+      return false;
+    }
+  }
+  for (let k in map) {
+    if (map[k] !== 0) return false;
+  }
+  return true;
+};
+
+// 9/16/2016
+var isAnagram = function(s, t) {
+  const l1 = s.length;
+  const l2 = t.length;
+  if (l1 !== l2) return false;
+  const map = {};
+  for (let i = 0; i < l1; i++) {
+    if (s[i] in map) {
+      map[s[i]]++;
+    } else {
+      map[s[i]] = 1;
+    }
+  }
+  for (let i = 0; i < l2; i++) {
+    if (t[i] in map) {
+      map[t[i]]--;
+    } else {
+      return false;
+    }
+  }
+  return Object.keys(map).every(item => map[item] === 0);
+};
