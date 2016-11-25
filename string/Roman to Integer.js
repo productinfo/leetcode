b64e8328133d7f772b83c5c80db17b6222c2bb4e
LeoAJ <leo0924@gmail.com>
2016-11-24 23:18:36 -0800

    upload

--- string/Roman to Integer.js
+++ string/Roman to Integer.js
@@ -33,3 +33,24 @@ var romanToInt = function(s) {
     return total;
 
 };
+
+//9/16/2016
+var map = {
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  'C': 100,
+  'D': 500,
+  'M': 1000
+};
+var romanToInt = function(s) {
+  const len = s.length;
+  if (!len) return -1;
+  const res = 0;
+  for (let i = 0; i < len; i++) {
+    res += map[s[i]];
+    if (i > 0 && map[s[i - 1]] > map[s[i]]) res -= 2 * map[s[i - 1]];
+  }
+  return res;
+};
