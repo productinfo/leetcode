7d062a8b7b4fe2215986aeb942836fa091462f83
LeoAJ <leo0924@gmail.com>
2016-11-26 13:38:19 -0800

    upload

--- string/Palindrome Permutation.js
+++ string/Palindrome Permutation.js
@@ -75,3 +75,57 @@ var is = function (str) {
   return true;
 
 };
+
+// 9/16/2016
+const fb = (str) => {
+  const len = str.length;
+  if (!len) return false;
+  const map = {};
+  for (let i = 0; i < len; i++) {
+    if (str[i] in map) {
+      map[str[i]]++;
+    } else {
+      map[str[i]] = 1;
+    }
+  }
+  let even = 0;
+  Object.keys(map).forEach(k => {
+    if (map[k] % 2 !== 0) {
+      even++;
+    }
+  });
+	return len % 2 === 0 ? even === 0 : even === 1;
+};
+
+// 10/24/2016
+const f = str => {
+  const len = str.length;
+  const map = {};
+  for (let i = 0; i < len; i++) {
+    if (map[str[i]]) {
+      delete map[str[i]];
+    } else {
+      map[str[i]] = 1;
+    }
+  }
+  // const l = Object.keys(map).length;
+  // let odd = Object.keys(map).reduce((total, key) => (map[key] % 2 !== 0 && total++, total), 0);
+  return Object.keys(map).length <= 1;
+};
+
+// 10/25/2016
+const f = s => {
+  const len = s.length;
+  if (!len) return '';
+  const map = {};
+  let odd = 0;
+  for (let i = 0; i < len; i++) {
+    if (map[s[i]]) {
+      map[s[i]]++;
+    } else {
+      map[s[i]] = 1;
+    }
+    odd += map[s[i]] % 2 === 0 ? -1 : 1;
+  }
+  return odd <= 1;
+};
