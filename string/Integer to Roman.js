cc795d99edce6cf39f713fda15d1f8ccf55ab5ff
LeoAJ <leo0924@gmail.com>
2016-12-04 22:08:53 -0800

    upload

--- string/Integer to Roman.js
+++ string/Integer to Roman.js
@@ -21,3 +21,17 @@ var intToRoman = function(num) {
 
   return result;
 };
+
+// 9/16/2016
+var intToRoman = function(num) {
+  const symbols = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+  const vals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let res = '';
+  for (let i = 0; i < vals.length; i++) {
+    while (num >= vals[i]) {
+      num -= vals[i]
+      res += symbols[i];
+    }
+  }
+  return res;
+};
