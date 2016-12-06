f6331e6d5a6fa162895685f3812956d40ea9b711
LeoAJ <leo0924@gmail.com>
2016-12-06 10:21:11 -0800

    upload

--- string/Group Anagrams.js
+++ string/Group Anagrams.js
@@ -56,3 +56,36 @@ var groupAnagrams = function(strs) {
 
   return r;
 };
+
+// 9/15/2016
+var groupAnagrams = function(strs) {
+  const len = strs.length;
+  if (!len) return [];
+  const map = {};
+  for (let i = 0; i < len; i++) {
+    const k = strs[i].split('').sort().join('');
+    if (map[k]) {
+      map[k].push(strs[i]);
+    } else {
+      map[k] = [strs[i]];
+    }
+  }
+  return Object.keys(map).reduce((acc, k) => {
+    acc.push(map[k]);
+    return acc;
+  }, []);
+};
+
+// 11/26/2016
+const groupAnagrams = strs => {
+  const map = {};
+  for (let l = strs.length, i = 0; i < l; i++) {
+    const key = strs[i].split('').sort().join('');
+    if (map[key]) {
+      map[key].push(strs[i]);
+    } else {
+      map[key] = [strs[i]];
+    }
+  }
+  return Object.keys(map).reduce((result, key) => (result.push(map[key]), result),[])
+};
