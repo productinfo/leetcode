0116ab853f5de363d943f86286d91b27798755f7
LeoAJ <leo0924@gmail.com>
2016-12-15 11:47:44 -0800

    upload

--- array/Summary Ranges.js
+++ array/Summary Ranges.js
@@ -102,3 +102,57 @@ const z = (n) => {
 
 	return r;
 };
+
+// 9/13/2016
+var summaryRanges = function(nums) {
+  const res = [];
+  const len = nums.length;
+  if (!len) return res;
+  if (len === 1) return [print(nums[0], nums[0])];
+  let start = nums[0];
+  for (let i = 0; i < len; i++) {
+    if (nums[i + 1] && nums[i] + 1 === nums[i + 1]) {
+      continue;
+    }
+    res.push(print(start, nums[i]));
+    start = nums[i + 1];
+  }
+	return res;
+};
+
+const print = (start, end) => start === end
+  ? `${start}`
+  : `${start}->${end}`;
+
+// 10/24/2016
+const print = (s, e) => s === e ? `${s}` : `${s}->${e}`;
+const summaryRanges = arr => {
+  const res = [];
+  const len = arr.length;
+  if (!len) return res;
+  let p = 0;
+  for (let i = 0; i < len - 1; i++) {
+    if (arr[i] + 1 === arr[i + 1]) continue;
+    res.push(print(arr[p], arr[i]));
+    p = i + 1;
+  }
+  res.push(print(arr[p], arr[len - 1]));
+  return res;
+};
+
+// 11/30/2016
+const pr = (v1, v2) => v1 === v2 ? `${v1}` : `${v1}->${v2}`;
+
+const summaryRanges = arr => {
+  const res = [];
+  const l = arr.length;
+  if (!l) return res;
+  let next = arr[0];
+  for (let i = 1; i < l; i++) {
+    if (arr[i - 1] + 1 === arr[i]) continue;
+    res.push(pr(next, arr[i - 1]));
+    next = arr[i];
+  }
+  if (next <= arr[l - 1]) res.push(pr(next, arr[l - 1]));
+  return res;
+};
