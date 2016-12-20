e85739e4b95bfb45bdd9b627e199fd457adc2028
LeoAJ <leo0924@gmail.com>
2016-12-20 09:49:23 -0800

    upload

--- array/Search in Rotated Sorted Array II.js
+++ array/Search in Rotated Sorted Array II.js
@@ -16,3 +16,30 @@ var search = function(A, target) {
   return false;
 
 };
+
+// 11/5/2016
+const search = (arr, target) => {
+  let l = 0;
+  let h = arr.length - 1;
+  while (l <= h) {
+    let m = (l + h) >> 1;
+    if (arr[m] === target) {
+      return true;
+    } else if (arr[l] === arr[m]) {
+      l++;
+    } else if (arr[l] <= arr[m]) {
+      if (arr[l] <= target && target < arr[m]) {
+        h = m - 1;
+      } else {
+        l = m + 1
+      }
+    } else {
+      if (arr[m] < target && target <= arr[h]) {
+        l = m + 1;
+      } else {
+        h = m - 1;
+      }
+    }
+  }
+  return false;
+}
