f80645a121c1c837c1312448f0ae3a985118e2d0
LeoAJ <leo0924@gmail.com>
2016-12-28 22:44:57 -0800

    upload

--- array/Search for a Range.js
+++ array/Search for a Range.js
@@ -13,7 +13,7 @@ var searchRange = function(A, target) {
 
     if (target === A[mid]) {
       // return mid;
-      
+
       var left = mid, right = mid;
 
       while (left >= 0 && A[left] === target) {
@@ -36,4 +36,77 @@ var searchRange = function(A, target) {
 
   return [-1, -1];
 
+};
+
+// 9/12/2016
+var searchRange = function(nums, target) {
+  let low = 0;
+  let high = nums.length - 1;
+  while (low <= high) {
+    let mid = (low + high) >> 1;
+    if (nums[mid] === target) {
+      let i = mid;
+      let j = mid;
+      while (i >= 0 &&  nums[i] === target) {
+        i--;
+      }
+      while (j < nums.length && nums[j] === target) {
+        j++;
+      }
+      return [i + 1, j - 1];
+    } else if (nums[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return [-1, -1];
+};
+
+// 10/7/2016
+var searchRange = function(nums, target) {
+  const len = nums.length;
+  if (!len) return [-1, -1];
+  let l = 0;
+  let h = len - 1;
+  while (l <= h) {
+    let m = (l + h) >> 1;
+    if (nums[m] === target) {
+      let i = m;
+      let j = m;
+      while (i >= 0 && nums[i] === nums[m]) i--;
+      while (j < len && nums[j] === nums[m]) j++;
+      return [i + 1, j - 1];
+    } else if (nums[m] < target) {
+      l = m + 1;
+    } else {
+      h = m - 1;
+    }
+  }
+  return [-1, -1];
+};
+
+// 10/21/2016
+const searchRange = (arr, target) => {
+  const noResult = [-1, -1];
+  if (!arr || target === undefined || !Array.isArray(arr)) return noResult;
+  const len = arr.length;
+  if (!len) return noResult;
+  let l = 0;
+  let h = len - 1;
+  while (l <= h) {
+    let m = (l + h) >> 1;
+    if (arr[m] === target) {
+      let i = m;
+      let j = m;
+      while (i >= 0 && arr[i] === target) i--;
+      while (j < len && arr[j] === target) j++;
+      return [i + 1, j - 1];
+    } else if (arr[m] < target) {
+      l = m + 1;
+    } else {
+      h = m - 1;
+    }
+  }
+  return noResult;
 };
