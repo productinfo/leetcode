444ec73f6dfa852490c92757d48abd5f0928fc44
LeoAJ <leo0924@gmail.com>
2017-01-06 15:21:43 -0800

    upload

--- array/Minimum Path Sum.js
+++ array/Minimum Path Sum.js
@@ -48,3 +48,21 @@ const minPathSum = (g) => {
 
   return dp[xl - 1];
 };
+
+// 9/25/2016
+var minPathSum = function(grid) {
+  const yl = grid.length;
+  const xl = grid[0].length;
+	const dp = grid[0].slice();
+  for (let x = 1; x < xl; x++) {
+    dp[x] += dp[x - 1];
+  }
+  // console.log(dp);
+  for (let y = 1; y < yl; y++) {
+    dp[0] += grid[y][0];
+    for (let x = 1; x < xl; x++) {
+      dp[x] = Math.min(dp[x - 1], dp[x]) + grid[y][x];
+    }
+  }
+  return dp[xl - 1];
+};
