d6f1161108861be14a2a02152a39791c856951bf
LeoAJ <leo0924@gmail.com>
2017-01-20 11:01:04 -0800

    upload

--- array/3Sum.js
+++ array/3Sum.js
@@ -45,3 +45,60 @@ var threeSum = function(nums) {
 
   return result;
 };
+
+// 9/23/2016
+const threeSum = arr => {
+  const res = [];
+  const len = arr.length;
+  if (!len) return res;
+  arr = arr.sort((a, b) => a - b);
+  for (let i = 0; i < len - 2; i++) {
+    if (i > 0 && arr[i] === arr[i - 1]) continue;
+    let b = i + 1;
+    let c = len - 1;
+    while (b < c) {
+      const sum = arr[i] + arr[b] + arr[c];
+      if (sum === 0) {
+        res.push([arr[i], arr[b], arr[c]]);
+        while (b < c && arr[b] === arr[b + 1]) b++;
+        while (b < c && arr[b] === arr[c - 1]) c--;
+        b++;
+        c--;
+      } else if (sum < 0) {
+        b++;
+      } else {
+        c--;
+      }
+    }
+  }
+  return res;
+};
+
+// 10/20/2016
+const threeSum = arr => {
+  const res = [];
+  if (!Array.isArray(arr)) return res;
+  const len = arr.length;
+  if (!len) return res;
+  arr = arr.sort((a, b) => a - b);
+  for (let i = 0; i < len - 2; i++) {
+    if (i > 0 && arr[i] === arr[i - 1]) continue;
+    let j = i + 1;
+    let k = len - 1;
+    while (j < k) {
+      const sum = arr[i] + arr[j] + arr[k];
+      if (sum === 0) {
+        res.push([arr[i], arr[j], arr[k]]);
+        while (j < k && arr[j] === arr[j + 1]) j++;
+        while (j < k && arr[k] === arr[k - 1]) k--;
+        j++;
+        k--;
+      } else if (sum < 0) {
+        j++;
+      } else {
+        k--;
+      }
+    }
+  }
+  return res;
+};
