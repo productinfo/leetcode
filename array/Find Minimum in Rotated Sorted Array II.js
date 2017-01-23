9a9f7bb17bdc3f9be6017daa6fe9d4dd0e528642
LeoAJ <leo0924@gmail.com>
2017-01-22 16:03:52 -0800

    upload

--- array/Find Minimum in Rotated Sorted Array II.js
+++ array/Find Minimum in Rotated Sorted Array II.js
@@ -36,3 +36,44 @@ var findMin = function(nums) {
   return go(0, l - 1);
 
 };
+
+// 9/8/2016
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+  let low = 0;
+  let high = nums.length - 1;
+  let mid;
+  while (low < high) {
+    mid = (low + high) >> 1;
+    if (nums[mid] > nums[high]) {
+      low = mid + 1;
+    } else if (nums[mid] < nums[high]) {
+      high = mid;
+    } else {
+      high--;
+    }
+  }
+  return nums[low];
+};
+
+// 11/15/2016
+const findMin = arr => {
+  let l = 0;
+  let h = arr.length - 1;
+  while (l < h) {
+    let m = (l + h) >> 1;
+    if (arr[l] < arr[h]) {
+      return arr[l];
+    } else if (arr[l] === arr[m]) {
+      l++;
+    } else if (arr[l] <= arr[m]) {
+      l = m + 1;
+    } else {
+      h = m;
+    }
+  }
+  return arr[l];
+};
