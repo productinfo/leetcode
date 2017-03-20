2a42f255f13b400deb571328262414ab28c7507f
LeoAJ <leo0924@gmail.com>
2017-03-20 09:19:13 -0700

    upload

--- Other/Implement Stack using Queues.js
+++ Other/Implement Stack using Queues.js
@@ -43,3 +43,26 @@ Stack.prototype.top = function() {
 Stack.prototype.empty = function() {
   return this.q.length === 0;
 };
+
+// 11/30/2016
+class Stack {
+  constructor() {
+    this.q = [];
+  }
+  push(x) {
+    const tmpq = [x];
+    while (this.q.length) {
+      tmpq.push(this.q.shift());
+    }
+    this.q = tmpq;
+  }
+  pop() {
+    return this.q && this.q.shift();
+  }
+  top() {
+    return this.q && this.q[0];
+  }
+  empty() {
+    return this.q.length === 0;
+  }
+}
