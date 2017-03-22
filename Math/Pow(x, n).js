026d6f7d4d3599e4090514405972292908fb82ea
LeoAJ <leo0924@gmail.com>
2017-03-22 08:16:27 -0700

    upload

--- Math/Pow(x, n).js
+++ Math/Pow(x, n).js
@@ -17,3 +17,11 @@ var myPow = function(x, n) {
   }
 
 };
+
+// 11/27/2016
+const myPow = (x, n) => {
+  if (n === -1) return 1 / x;
+  if (n === 0) return 1;
+  const tmp = myPow(x, n >> 1);
+  return n % 2 === 0 ? tmp * tmp : tmp * tmp * x;
+};
