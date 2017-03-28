18fa02b5c6f8e9e3ce65f0dbfb2ff2da8db97782
LeoAJ <leo0924@gmail.com>
2017-03-28 09:48:39 -0700

    upload

--- String/Palindrome Permutation II.js
+++ String/Palindrome Permutation II.js
@@ -74,3 +74,64 @@ console.log(g('aabbcadad'));
 // console.log(g('carerac'));
 // g('abc');
 // g('carerac');
+
+// 11/23/2016
+const generate = str => {
+  const l = str.length;
+  const res = [];
+  const sub = [];
+  const used = [];
+  str = str.sort((a, b) => a - b);
+  const dfs = _ => {
+    if (sub.length === l) {
+      res.push(sub.join(''));
+      return;
+    }
+    for (let i = 0; i < l; i++) {
+      if (used[i]) continue;
+      if (i > 0 && str[i] === str[i - 1] && used[i - 1]) continue;
+      sub.push(str[i]);
+      used[i] = true;
+      dfs();
+      sub.pop();
+      used[i] = false; 
+    }
+  };
+  dfs();
+  return res;
+};
+
+const g = str => {
+  const res = [];
+  const len = str.length;
+  const map = {};
+  let odd = 0;
+  for (let i = 0; i < len; i++) {
+    if (map[str[i]]) {
+      map[str[i]]++;
+    } else {
+      map[str[i]] = 1;
+    }
+    odd += map[str[i]] % 2 === 0 ? -1 : 1;
+  }
+  if (odd > 1) return res;
+  let mid = '';
+  let vals = [];
+  Object.keys(map).forEach(k => {
+    let num;
+    if (map[k] % 2 === 1) {
+      mid = k;
+      if (map[k] > 1) {
+        num = (map[k] - 1) / 2;
+      }
+    } else {
+      num = map[k] / 2;
+    }
+    vals = vals.concat(Array.from({ length: num }, v => k));
+  });
+  const list = generate(vals);
+  for (let ll = list.length, i = 0; i < ll; i++) {
+    res.push(`${list[i]}${mid}${list[i].split('').reverse().join('')}`);
+  }
+  return res;
+};
