901486f919426228ba7ebfe8d194ddaa8a10b1ab
LeoAJ <leo0924@gmail.com>
2017-04-05 10:00:33 -0700

    upload

---
+++ Hash/Maximum Size Subarray Sum Equals k.js
@@ -0,0 +1,15 @@
+const maxSubArrayLen = (nums, k) => {
+  const l = nums.length;
+  if (!l) return 0;
+  const map = {};
+  let currSum = 0;
+  let max = 0;
+  for (let i = 0; i < l; i++) {
+    currSum += nums[i];
+    const prevSum = currSum - k;
+    if (currSum === k) max = i + 1;
+    else if (prevSum in map) max = Math.max(max, i - map[prevSum]);
+    if (!(currSum in map)) map[currSum] = i;
+  }
+  return max;
+};
