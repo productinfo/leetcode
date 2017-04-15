b8f50fa102c14c4a941aa45b5e57611ce222619b
LeoAJ <leo0924@gmail.com>
2017-04-15 13:56:36 -0700

    upload

---
+++ DP/Regular Expression Matching.js
@@ -0,0 +1,90 @@
+// const isMatch = (s, p) => {
+//   const dp = [];
+//   for (let i = 0; i <= s.length; i++) {
+//     dp.push([]);
+//   }
+//   dp[0][0] = true;
+//   for (let i = 0; i < p.length; i++) {
+//     if (p[i] === '*' && dp[0][i - 1]) {
+//       dp[0][i + 1] = true;
+//     }
+//   }
+//   for (let y = 0; y < s.length; y++) {
+//     for (let x = 0; x < p.length; x++) {
+//       if (p[x] === '.' || p[x] === s[y]) {
+//         dp[y + 1][x + 1] = dp[y][x];
+//       } else if (p[x] === '*') {
+//         if (p[x - 1] !== s[y] && p[x - 1] !== '.') {
+//           dp[y + 1][x + 1] = dp[y + 1][x - 1];
+//         } else {
+//           dp[y + 1][x + 1] = dp[y + 1][x] || dp[y][x + 1] || dp[y + 1][x - 1];
+//         }
+//       }
+//     }
+//   }
+//   return !!dp[s.length][p.length];
+// };
+
+// const isMatch = (s, p) => {
+//   const dp = [];
+//   for (let i = 0; i <= s.length; i++) {
+//     dp.push([]);
+//   }
+//   dp[0][0] = true;
+//   for (let i = 1; i <= p.length; i++) {
+//     if (p[i - 1] === '*') {
+//       if (dp[0][i - 1] || (i > 1 && dp[0][i - 2])) {
+//         dp[0][i] = true;
+//       }
+//     }
+//   }
+
+//   for (let y = 1; y <= s.length; y++) {
+//     for (let x = 1; x <= p.length; x++) {
+//       if (s[y - 1] === p[x - 1] || p[x - 1] === '.') {
+//         dp[y][x] = dp[y - 1][x - 1];
+//       } else if (p[x - 1] === '*') {
+//         if (s[y - 1] !== p[x - 2] && p[x - 2] !== '.') {
+//           dp[y][x] = dp[y][x - 2];
+//         } else {
+//           dp[y][x] = dp[y - 1][x] || dp[y][x - 1] || dp[y][x - 2];
+//         }
+//       }
+//     }
+//   }
+//   return !!dp[s.length][p.length];
+// }
+
+// DP
+// O(mn)
+const isMatch = (s, p) => {
+  const dp = [];
+  for (let i = 0; i <= s.length; i++) {
+    dp.push([]);
+  }
+  dp[0][0] = true;
+  for (let i = 1; i <= p.length; i++) {
+    if (p[i - 1] === '*') {
+      dp[0][i] = dp[0][i - 2];
+    }
+  }
+  for (let y = 1; y <= s.length; y++) {
+    for (let x = 1; x <= p.length; x++) {
+      if (s[y - 1] === p[x - 1] || p[x - 1] === '.') {
+        // text are the same or pattern is `.
+        dp[y][x] = dp[y - 1][x - 1];
+      } else if (p[x - 1] === '*') {
+        // pattern is `.`
+        // two scenario
+        if (s[y - 1] !== p[x - 2] && p[x - 2] !== '.') {
+          // `*` consider zero case
+          dp[y][x] = dp[y][x - 2];
+        } else {
+          // `*` consider more case
+          dp[y][x] = dp[y][x - 2] || dp[y - 1][x] || dp[y][x - 1];
+        }
+      }
+    }
+  }
+  return !!dp[s.length][p.length];
+}
