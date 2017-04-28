0b243d8a439de3ad90e956f3bc4eead907d78987
LeoAJ <leo0924@gmail.com>
2017-04-28 11:16:05 -0700

    upload

---
+++ Myself/Given a list of number, there is only one peak or one drop. Find the maximum drop.js
@@ -0,0 +1,43 @@
+/*
+Given a list of number, there is only one peak or one drop. Find the maximum drop.
+Exps:
+1 -> 2 -> 3 -> 9 -> 3 -> 0 = 9;
+10 -> 4 -> 3 -> 8 = 7 ;
+*/
+/**
+ For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+ */
+var findPeakElement = function(nums) {
+  let l = 0;
+  let h = nums.length - 1;
+  while (l < h) {
+    let m = (l + h) >> 1;
+    if (nums[m] > nums[m + 1]) {
+      h = m;
+    } else {
+      l = m + 1;
+    }
+  }
+  return l;
+};
+const y = arr => {
+  let l = 0;
+  let h = arr.length - 1;
+  while (l < h) {
+    let m = (l + h) >> 1;
+    if (arr[m] < arr[m + 1]) {
+      h = m;
+    } else {
+      l = m + 1;
+    }
+  }
+  return arr[l];
+};
+const x = arr => {
+	const maxIndex = findPeakElement(arr);
+	const min = y(arr.slice(maxIndex));
+	return arr[maxIndex] - min;
+};
+
+console.log(x([1, 2, 3, 9, 3, 0]));
+console.log(x([10, 4, 3, 8]));
