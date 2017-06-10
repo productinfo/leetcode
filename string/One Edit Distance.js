6fca76f6259582183409b343d562ed49b07c62ef
LeoAJ <leo0924@gmail.com>
2017-06-10 09:20:18 -0700

    upload

--- string/One Edit Distance.js
+++ string/One Edit Distance.js
@@ -25,3 +25,61 @@ var isOneEditDistance = function (s, t) {
   return i === m;
 
 }
+
+// 4/5/2017
+const isOneModified = (a, b) => {
+  let modified = false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      if (modified) return false;
+      modified = true;
+    }
+  }
+  return modified;
+};
+
+const isOneDeleted = (longer, shorter) => {
+  for (let i = 0; i < shorter.length; i++) {
+    if (longer[i] !== shorter[i]) {
+      return longer.substring(i + 1) === shorter.substring(i);
+    }
+  }
+  return true;
+};
+
+const isOneEditDistance = (s, t) => {
+  const sl = s.length;
+  const tl = t.length;
+  if (sl === tl) return isOneModified(s, t);
+  if (sl - tl === 1) return isOneDeleted(s, t);
+  if (tl - sl === 1) return isOneDeleted(t, s);
+  return false;
+};
+
+// 4/12/2017
+const checkOneCharDiff = (longer, shorter) => {
+  for (let i = 0; i < shorter.length; i++) {
+    if (shorter[i] !== longer[i]) {
+      return longer.substring(i + 1) === shorter.substring(i);
+    }
+  }
+  return true;
+};
+
+const checkSameLength = (s, t) => {
+  let diff = 0;
+  for (let i = 0; i < t.length; i++) {
+    if (s[i] !== t[i]) diff += 1;
+  }
+  return diff === 1;
+};
+
+const isOneEditDistance = (s, t) => {
+  if (typeof s !== 'string' || typeof t !== 'string') return false;
+  const sl = s.length;
+  const tl = t.length;
+  if (sl === tl) return checkSameLength(s, t);
+  if (sl - tl === 1) return checkOneCharDiff(s, t);
+  if (tl - sl === 1) return checkOneCharDiff(t, s);
+  return false;
+};
