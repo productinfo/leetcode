d49cafa029932848b3985e13410d73aba040773c
LeoAJ <leo0924@gmail.com>
2017-06-18 11:13:19 -0700

    upload

--- string/Decode Ways.js
+++ string/Decode Ways.js
@@ -1,3 +1,8 @@
+// many courner case
+// '00'
+// '10'
+// '11'
+
 /**
  * @param {string} s
  * @return {number}
@@ -57,4 +62,60 @@ var numDecodings = function(s) {
     }
   }
   return dp[len];
+};
+
+// 3/28/2017
+const getValue = (d1, d2) => +d1 * 10 + (+d2);
+const numDecodings = s => {
+  const len = s.length;
+  if (!len) return 0
+  const dp = [1, s[0] === '0' ? 0 : 1];
+  for (let i = 2; i <= len; i++) {
+    dp[i] = s[i - 1] === '0' ? 0 : dp[i - 1];
+    const val = getValue(s[i - 2], s[i - 1]);
+    if (9 < val && val < 27) dp[i] += dp[i - 2];
+  }
+  return dp[len];
+};
+
+// no array
+const getValue = (d1, d2) => +d1 * 10 + (+d2);
+const numDecodings = s => {
+  const len = s.length;
+  if (!len) return 0
+  let v1 = 1;
+  let v2 = s[0] === '0' ? 0 : 1;
+  if (len === 1) return v2;
+  let v3 = 0;
+  for (let i = 2; i <= len; i++) {
+    v3 = s[i - 1] === '0' ? 0 : v2;
+    const val = getValue(s[i - 2], s[i - 1]);
+    if (9 < val && val < 27) v3 += v1;
+    v1 = v2;
+    v2 = v3;
+  }
+  return v3;
+};
+
+// 6/1/2017
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+  const l = s.length;
+  if (!l) return 0;
+  let v1 = 1;
+  let v2 = s[0] === '0' ? 0 : 1;
+  if (l === 1) return v2;
+  const get = (m, n) => +m * 10 + +n;
+  let v3 = 0;
+  for (let i = 2; i <= l; i++) {
+    v3 = s[i - 1] !== '0' ? v2 : 0;
+    const val = get(s[i - 2], s[i - 1]);
+    if (val > 9 && val < 27) v3 += v1;
+    v1 = v2;
+    v2 = v3;
+  }
+  return v3;
 };
