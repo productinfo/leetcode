97bc7fa0c5c997005a86a297b596eb70c4deabee
LeoAJ <leo0924@gmail.com>
2017-06-22 09:04:23 -0700

    upload

--- array/Sort Colors.js
+++ array/Sort Colors.js
@@ -18,7 +18,7 @@ var sortColors = function(A) {
 
   while (index <= blue) {
 
-    if (A[index] === 0 && index > red) {
+    if (A[index] === 0 && red < index) {
       swap(A, index, red++);
     } else if (A[index] === 2) {
       swap(A, index, blue--);
@@ -35,3 +35,32 @@ function swap (A, x, y) {
   A[x] = A[y];
   A[y] = tmp;
 }
+
+// 3/26/2017
+const swap = (arr, m, n) => {
+  const tmp = arr[m];
+  arr[m] = arr[n];
+  arr[n] = tmp;  
+}
+
+const sortColors = (arr) => {
+  const len = arr.length;
+  let z = 0;
+  let t = len - 1;
+  while (z < len && arr[z] === 0) {
+    z++;
+  }
+  while (t >= 0 && arr[t] === 2) {
+    t--;
+  }
+  let i = z;
+  while (i <= t) {
+    if (arr[i] === 0 && z < i) {
+      swap(arr, z++, i);
+    } else if (arr[i] === 2) {
+      swap(arr, t--, i);
+    } else {
+      i++;
+    }
+  }
+};
