8c8971b1410c98e5660c4d74b254c09e4a8dfceb
LeoAJ <leo0924@gmail.com>
2017-06-25 10:24:49 -0700

    upload

--- array/Merge Sorted Array.js
+++ array/Merge Sorted Array.js
@@ -32,3 +32,40 @@ var merge = function(A, m, B, n) {
   }
 
 };
+
+// 3/27/2017
+const merge = (n1, m, n2, n) => {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+  while (i >= 0 && j >= 0) {
+    if (n1[i] < n2[j]) {
+      n1[k--] = n2[j--];
+    } else {
+      n1[k--] = n1[i--];
+    }
+  }
+  while (j >= 0) {
+    n1[k--] = n2[j--];
+  }
+}
+
+// standard version
+const merge = (n1, m, n2, n) => {
+  const res = [];
+  let i = 0, j = 0;
+  while (i < m && j < n) {
+    if (n1[i] < n2[j]) {
+      res.push(n1[i++]);
+    } else {
+      res.push(n2[j++]);
+    }
+  }
+	while (i < m) {
+		res.push(n1[i++]);
+	}
+	while (j < n) {
+		res.push(n2[j++]);
+	}
+  return res;
+};
