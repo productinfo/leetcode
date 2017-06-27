3f5a973caccdab75a03bd3c220c08f1d486d6215
LeoAJ <leo0924@gmail.com>
2017-06-27 11:42:46 -0700

    upload

--- array/Meeting Rooms II.js
+++ array/Meeting Rooms II.js
@@ -43,3 +43,46 @@ var minMeetingRooms = function (intervals) {
   return rooms.length;
 
 };
+
+// use heap
+// 4/4/2017
+const minMeetingRooms = intervals => {
+  const len = intervals.length;
+  if (!len) return 0;
+  intervals = intervals.sort((a, b) => a.start - b.start);
+  let q = [intervals[0].end];
+  for (let i = 1; i < len; i++) {
+    q = q.sort((a, b) => a - b);
+    if (intervals[i].start >= q[0]) {
+        q.shift();
+    }
+    q.push(intervals[i].end);
+  }
+  return q.length;
+};
+
+// 6/3/2017
+/**
+ total // O(nlogn)
+ */
+/**
+ * @param {Interval[]} intervals
+ * @return {number}
+ */
+var minMeetingRooms = function(intervals) {
+  const len = intervals.length;
+  if (!len) return 0;
+  // O(nlogn)
+  intervals = intervals.sort((a, b) => a.start - b.start);
+  const h = new Heap();
+  // O(1)
+  h.add(intervals[0].end);
+  // O(n)
+  for (let i = 1; i < len; i++) {
+    // O(logn)
+    if (intervals[i].start >= h.getMin()) h.remove();
+    // O(logn)
+    h.add(intervals[i].end);
+  }
+  return h.total();
+};
