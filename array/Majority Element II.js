373a6f812eacca862d9bc8aa26816085c555813e
LeoAJ <leo0924@gmail.com>
2017-06-29 09:30:54 -0700

    upload

--- array/Majority Element II.js
+++ array/Majority Element II.js
@@ -46,3 +46,42 @@ var majorityElement = function(nums) {
   return res;
 
 };
+
+// 6/4/2017
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var majorityElement = function(nums) {
+  const len = nums.length;
+  let n1;
+  let n2;
+  let c1 = 0;
+  let c2 = 0;
+  for (const v of nums) {
+    if (n1 && n1 === v) {
+      c1++;
+    } else if (n2 && n2 === v) {
+      c2++;
+    } else if (c1 === 0) {
+      c1++;
+      n1 = v;
+    } else if (c2 === 0) {
+      c2++;
+      n2 = v;
+    } else {
+      c1--;
+      c2--;
+    }
+  }
+  c1 = 0;
+  c2 = 0;
+  for (const v of nums) {
+    if (v === n1) c1++;
+    else if (v === n2) c2++;
+  }
+  const res = [];
+  if (c1 > (len / 3)) res.push(n1);
+  if (c2 > (len / 3)) res.push(n2);
+  return res;
+};
