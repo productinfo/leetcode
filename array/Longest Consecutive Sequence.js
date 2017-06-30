81ac7a6eadccdeb9e8e2e05579155ac6633f9a13
LeoAJ <leo0924@gmail.com>
2017-06-30 12:44:02 -0700

    upload

--- array/Longest Consecutive Sequence.js
+++ array/Longest Consecutive Sequence.js
@@ -74,3 +74,56 @@ const longestConsecutive = arr => {
   arr.forEach(v => max = Math.max(max, go(v, false) + go(v + 1, true)));
   return max;
 };
+
+// 3/27/2017
+// [100, 4, 200, 1, 3, 2]
+// 1 , 2, 3, 4
+// 4
+const longestConsecutive = arr => {
+  const map = {};
+  arr.forEach(a => map[a] = true);
+  const find = (v, inc) => {
+    let t = 0;
+    while (v in map) {
+      delete map[v];
+      t += 1;
+      if (inc) {
+        v += 1;
+      } else {
+        v -= 1;
+      }
+    }
+    return t;
+  };
+  let max = 0;
+  arr.forEach(a => max = Math.max(
+    max,
+    find(a, false) + find(a + 1, true)
+  ));
+  return max;
+};
+
+// 4/13/2017
+// [100, 4, 200, 1, 3, 2],
+const longestConsecutive = arr => {
+  const map = {};
+  let max = 0;
+  for (const a of arr) {
+    map[a] = true;
+  }
+  const go = (n, inc) => {
+    let t = 0;
+    while (n in map) {
+      delete map[n];
+      t += 1;
+      if (inc) n += 1;
+      else n -= 1;
+    }
+    return t;
+  };
+  for (const a of arr) {
+    const total = go(a, false) + go(a + 1, true);
+    max = Math.max(max, total);
+  }
+  return max;
+};
