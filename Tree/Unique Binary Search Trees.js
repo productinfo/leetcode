6340d5150a8470806f7b29961e4cfbc81db50dfd
LeoAJ <leo0924@gmail.com>
2017-07-12 13:32:14 -0700

    upload

--- Tree/Unique Binary Search Trees.js
+++ Tree/Unique Binary Search Trees.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var numTrees = function(n) {
-    
+
   var c = [0, 1], i = 2, j;
 
   for (i ; i < n ; i++) {
@@ -18,4 +18,16 @@ var numTrees = function(n) {
 
   return c[n];
 
+};
+
+// 10/23/2016
+const numTrees = n => {
+  const t = [1, 1];
+  for (let i = 2; i <= n; i++) {
+    t[i] = 0;
+    for (let j = 0; j < i; j++) {
+      t[i] += t[j] * t[i - j - 1];
+    }
+  }
+  return t[n];
 };
