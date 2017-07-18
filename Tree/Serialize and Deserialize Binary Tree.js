77d2db60661c1a718c69b0332a1daf670deb7000
LeoAJ <leo0924@gmail.com>
2017-07-17 23:39:49 -0700

    upload

--- Tree/Serialize and Deserialize Binary Tree.js
+++ Tree/Serialize and Deserialize Binary Tree.js
@@ -41,3 +41,49 @@ const deserialize = data => {
   };
   return build();
 };
+
+// convert to Linked List
+const NULL = 'null';
+/**
+  
+ */
+var serialize = function(root) {
+  const dm = new ListNode(-1);
+  let cur = dm;
+  const go = r => {
+    if (r) {
+      cur.next = new ListNode(r.val);
+      cur = cur.next;
+      go(r.left);
+      go(r.right);
+    } else {
+      cur.next = new ListNode(NULL);
+      cur = cur.next;
+    }
+  };
+  go(root);
+  return dm.next;
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function(head) {
+  let cur = head;
+  const go = _ => {
+    if (cur.val === NULL) {
+      cur = cur.next;
+      return null;
+    } else {
+      const r = new TreeNode(cur.val);
+      cur = cur.next;
+      r.left = go();
+      r.right = go();
+      return r;
+    }
+  };
+  return go();
+};
