f1c987dc9ff173529ab86c67b2e2c20e5535f148
LeoAJ <leo0924@gmail.com>
2017-07-21 10:18:18 -0700

    upload

--- Tree/Populating Next Right Pointers in Each Node II.js
+++ Tree/Populating Next Right Pointers in Each Node II.js
@@ -60,3 +60,58 @@ var connect = function(root) {
   }
 
 };
+
+// 3/30/2017
+const connect = root => {
+  if (!root) return;
+  let cursor = root;
+  let leftMost = null;
+  let levelWalker = null;
+
+  while (cursor) {
+    while (cursor) { // iterate on the current level
+      // left child
+      if (cursor.left) {
+        if (levelWalker) {
+          levelWalker.next = cursor.left;
+        } else {
+          leftMost = cursor.left;
+        }
+        levelWalker = cursor.left;
+      }
+      // right child
+      if (cursor.right) {
+        if (levelWalker) {
+          levelWalker.next = cursor.right;
+        } else {
+          leftMost = cursor.right;
+        }
+        levelWalker = cursor.right;
+      }
+      cursor = cursor.next;
+    }
+    cursor = leftMost;
+    leftMost = null;
+    levelWalker = null;
+  }
+};
+
+// BFS
+// use Queue
+// time: O(n)
+// space: O(n)
+const connect = root => {
+  if (!root) return;
+  const q = [root];
+  while (q.length) {
+    const size = q.length;
+    let next = null;
+    for (const k = 0; k < size; k++) {
+      const n = q.shift();
+      n.next = next;
+      next = n;
+      if (n.right) q.push(n.right);
+      if (n.left) q.push(n.left);
+    }
+  }
+};
