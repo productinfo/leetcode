55b5e160999855140530602dba8bc90f6f4e8ead
LeoAJ <leo0924@gmail.com>
2017-07-23 08:45:52 -0700

    upload

--- Tree/Populating Next Right Pointers in Each Node.js
+++ Tree/Populating Next Right Pointers in Each Node.js
@@ -35,3 +35,30 @@ var connect = function(root) {
   }
 
 };
+
+// 11/11/2016
+const connect = root => {
+  if (!root) return;
+  let pre = root;
+  let cur = null;
+  while (pre.left) {
+    cur = pre;
+    while (cur) {
+      cur.left.next = cur.right;
+      if (cur.next) cur.right.next = cur.next.left;
+      cur = cur.next;
+    }
+    pre = pre.left;
+  }
+};
+
+// 3/30/2017
+const connect = root => {
+  if (!root) return;
+  if (root.left) {
+    root.left.next = root.right;
+    if (root.next) root.right.next = root.next.left;
+  }
+  connect(root.left);
+  connect(root.right);
+};
