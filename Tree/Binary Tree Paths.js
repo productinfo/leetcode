8dfa88b32f0778a29f4dd35a4d9c329e79bdb0e9
LeoAJ <leo0924@gmail.com>
2017-08-01 22:22:42 -0700

    upload

--- Tree/Binary Tree Paths.js
+++ Tree/Binary Tree Paths.js
@@ -46,3 +46,35 @@ var binaryTreePaths = function(root) {
   return res;
 
 };
+
+// 10/23/2016
+const binaryTreePaths = root => {
+  const res = [];
+  if (!root) return res;
+  const sub = [];
+  const dfs = r => {
+    if (!r) return;
+    sub.push(r.val);
+    if (!r.left && !r.right) {
+      res.push(sub.join('->'));
+      return;
+    }
+    if (r.left) {
+      dfs(r.left);
+      sub.pop();
+    }
+    if (r.right) {
+      dfs(r.right);
+      sub.pop();
+    }
+  };
+  dfs(root);
+  return res;
+};
+
+/*
+Though it seems that time complexity is O(N)
+but if you need to print all paths then it is O(N*logN).
+Suppose that u have a complete binary tree then the total paths will be N/2
+and each path will have logN nodes so total of O(N*logN) in worst case.
+*/
