46ec674632d0c0a2dccc1a9d05b6dd0a6d6f9d8e
LeoAJ <leo0924@gmail.com>
2017-08-08 16:30:52 -0700

    upload

--- Tree/Balanced Binary Tree.js
+++ Tree/Balanced Binary Tree.js
@@ -18,3 +18,19 @@ var isBalanced = function(root) {
     if (!root) return true;
     return Math.abs(getDepth(root.left) - getDepth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
 };
+
+// 10/25/2016
+const getHeight = r => r ? Math.max(getHeight(r.left), getHeight(r.right)) + 1 : 0;
+
+const isBalanced = root => {
+  if (!root) return true;
+  return Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+};
+
+// 11/27/2016
+const get = r => r
+  ? Math.max(get(r.left), get(r.right)) + 1
+  : 0;
+const isBalanced = root => root
+  ? Math.abs(get(root.left) - get(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+  : true;
