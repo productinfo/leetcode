7ac9abec7334ef4816d8b7f43c5897f3a9278bb8
LeoAJ <leo0924@gmail.com>
2017-08-21 12:37:51 -0700

    upload

--- Myself/find median in list of sorted arrays.js
+++ Myself/find median in list of sorted arrays.js
@@ -57,3 +57,51 @@ const y = arr => {
 	}
 	console.log(pv, md);
 };
+
+// 5/29/2017
+/*
+[1, 3, 5]
+[2, 4, 6]
+[7, 8, 9]
+
+*/
+
+// get number of median
+// push num into heap
+// pop out from heap for X many times (same as number of median)
+
+const x = arr => {
+  let total = arr.reduce((t, a) => t + a.length, 0);
+  const isEven = total % 2 === 0;
+  // let count = Math.round(total / 2);
+  let count = (total >> 1) + 1;
+  const heap = [];
+  for (let i = 0; i < arr.length; i++) {
+    heap.push({
+      val: arr[i][0],
+      index: i,
+      elemIndex: 0
+    });
+  }
+  let pv = 0;
+  let mid = 0;
+  while (count-- > 0) {
+    heap.sort((a, b) => a.val - b.val);
+    const node = heap.shift();
+    pv = mid;
+    mid = node.val;
+    heap.push({
+      val: arr[node.index][node.elemIndex + 1],
+      index: node.index,
+      elemIndex: node.elemIndex + 1
+    });
+  }
+  console.log(pv, mid);
+  return isEven ? (pv + mid) / 2 : mid;
+};
+
+x([
+  [1, 3, 5],
+  [2, 4, 6],
+  [7, 8, 9]
+])
