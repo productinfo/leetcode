fcfb7bbea8d3b0fc839dd7a7fef1aba77465998a
LeoAJ <leo0924@gmail.com>
2017-08-29 11:17:00 -0700

    upload

--- Other/H-Index.js
+++ Other/H-Index.js
@@ -13,3 +13,24 @@ const hIndex = citations => {
   }
   return 0;
 };
+
+// 8/20/2017
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+  const len = citations.length;
+  if (!len) return 0;
+  const tmp = Array.from({ length: len + 1 }, v => 0);
+  for (let i = 0; i < len; i++) {
+    if (citations[i] >= len) tmp[len]++;
+    else tmp[citations[i]]++;
+  }
+  let t = 0;
+  for (let i = tmp.length - 1; i >= 0; i--) {
+    t += tmp[i];
+    if (t >= i) return i;
+  }
+  return 0;
+};
