624248674a19718d643609c3aca7eb2d67c78ef7
LeoAJ <leo0924@gmail.com>
2017-09-06 12:21:31 -0700

    upload

--- Tree/Serialize and Deserialize Binary Tree.js
+++ Tree/Serialize and Deserialize Binary Tree.js
@@ -87,3 +87,37 @@ var deserialize = function(head) {
   };
   return go();
 };
+
+// 8/21/2017
+const N = 'null';
+var serialize = function(root) {
+  if (!root) return '';
+  const sub = [];
+  const go = r => {
+    if (r) {
+      sub.push(r.val);
+      go(r.left);
+      go(r.right);
+    } else {
+      sub.push(N);
+    }
+  };
+  go(root);
+  return sub.join(',');
+};
+var deserialize = function(data) {
+  if (!data.length) return null;
+  const g = data.split(',');
+  const b = _ => {
+    const node = g.shift();
+    if (node === N) {
+      return null;
+    } else {
+      const root = new TreeNode(+node);
+      root.left = b();
+      root.right = b();
+      return root;
+    }
+  };
+  return b();
+};
