c96a81d12969678856e76b1da88a525df2edbdb0
LeoAJ <leo0924@gmail.com>
2017-09-08 12:50:44 -0700

    upload

--- String/Valid Parentheses.js
+++ String/Valid Parentheses.js
@@ -100,3 +100,22 @@ const isValid = s => {
   }
   return stack.length === 0;
 };
+
+// 8/29/2017
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  const stack = [];
+  for (const ch of s) {
+    if (ch === '(' || ch === '[' || ch === '{') stack.push(ch);
+    else if (
+      ch === ')' && stack.length && stack[stack.length - 1] === '(' ||
+      ch === ']' && stack.length && stack[stack.length - 1] === '[' ||
+      ch === '}' && stack.length && stack[stack.length - 1] === '{'
+    ) stack.pop();
+    else return false;
+  }
+  return stack.length === 0;
+};
