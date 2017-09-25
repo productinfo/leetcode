2d4982ad5444a79244eb15af39ff24b193b957e0
LeoAJ <leo0924@gmail.com>
2017-09-24 21:23:38 -0700

    upload

--- Javascript/Given a node from a DOM tree find the node in the same position from an identical DOM tree.js
+++ Javascript/Given a node from a DOM tree find the node in the same position from an identical DOM tree.js
@@ -93,3 +93,47 @@ var answer3 = function (node, target) {
 // TEST CASE //
 var a1 = answer1(document.body, document.querySelector('#test'));
 var a2 = answer2(document.body, document.querySelector('#test'));
+
+// 5/7/2017
+const get = (A, B, x) => {
+  let tmp = x;
+  const stack = [];
+  while (tmp && !tmp.isSameNode(A)) {
+    if (!tmp.parentNode) break;
+    const child = tmp.parentNode.childNodes;
+    for (let j = 0; j < child.length; j++) {
+      if (child[j].nodeType === 1 && child[j].isSameNode(tmp)) {
+        stack.push(j);
+        tmp = child[j].parentNode;
+        break;
+      }
+    }
+  }
+  if (!stack.length) return null;
+  let cur = B;
+  while (cur && stack.length) {
+    cur = cur.childNodes[stack.pop()];
+  }
+  return cur;
+};
+
+// 5/9/2017
+const z = (A, B, x) => {
+  const stack = [];
+  while (x && !x.isSameNode(A)) {
+    if (!x.parentNode) break;
+    const child = x.parentNode.childNodes;
+    for (let j = 0; j < child.length; j++) {
+      if (child[j].nodeType === 1 && child[j].isSameNode(x)) {
+        stack.push(j);
+        x = x.parentNode;
+        break;
+      }
+    }
+  }
+  if (!stack.length) return null;
+  while (B && stack.length) {
+    B = B.childNodes[stack.pop()];
+  }
+  return B;
+};
