b83a7bfd3216a34e0204b43ad9b3a0f59fb15444
LeoAJ <leo0924@gmail.com>
2017-11-19 16:25:16 -0800

    upload

--- array/Intersection of Two Arrays.js
+++ array/Intersection of Two Arrays.js
@@ -7,3 +7,13 @@ const intersection = (a, b) => {
   });
   return Object.keys(map).reduce((acc, key) => (acc.push(+key), acc), []);
 };
+
+// 8/31/2017
+var intersection = function(nums1, nums2) {
+  const map = {};
+  for (const v1 of nums1) map[v1] = 0;
+  for (const v2 of nums2) {
+    if (v2 in map) map[v2]++;
+  }
+  return Object.keys(map).filter(v => map[v] > 0).map(c => +c);
+};
